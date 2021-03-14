e96619816f3ee7ed84c295638ff1f017ce3b17ca
Mariah Morales <mjustine622@gmail.com>
2021-03-14 16:14:54 -0400

    commit tree a8f48619f8853c8569c5bae5fc4a90eb41a01fde

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,27 @@ require 'pry'
 class String
 
   def sentence?
-    
+   self.end_with?(".")   
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
+    self.end_with?("!")
 
   end
 
   def count_sentences
-
-  end
+    string_sentences = self.split
+    sentences = 0
+    
+    string_sentences. each do |word|
+      if word.end_with?("!", "?", ".")
+        sentences += 1
+      end 
+    end
+    sentences
+  end 
 end
